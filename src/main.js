da5041fb420ae0dcf15bccfb40277758bafa2d3a
Chris Spohn <chris.j.spohn@gmail.com>
2020-10-22 12:36:17 -0600

    Add initial button functionality to reveal message and hide image

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
+//query selectors
+var msgButton = document.querySelector('button');
+var bellImage = document.querySelector('img');
+var message = document.querySelector('.message');
 
+//event listeners
+msgButton.addEventListener('click', displayMessage)
+
+//event handlers
+
+function displayMessage() {
+  if (!bellImage.classList.contains('hidden')) {
+    revealMessage();
+  }
+}
+
+function revealMessage() {
+  bellImage.classList.toggle('hidden');
+  message.classList.toggle('hidden');
+}
